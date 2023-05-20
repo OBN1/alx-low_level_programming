800a8007702427481f9dabee193e3b0bd6bc57bb
OBN1 <nyangaresibrian@gmail.com>
2023-05-19 17:53:37 -0700

    We are done with everything

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,25 +5,25 @@
  *
  * Description: Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0 (success)
+ * Return: (0) Always 0 (success)
  */
 int main(void)
 {
 	int num;
 
-    	for (num = 0; num <= 9; num++)
-    	{
+	for (num = 0; num <= 9; num++)
+	{
 		putchar(num + '0');
 
-        	if (num != 9)
-        	{
-            		putchar(',');
-            		putchar(' ');
-        	}
-    	}
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    	putchar('\n');
+	putchar('\n');
 
-    	return 0;
+	return (0);
 }
 
