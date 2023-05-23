6e99dc474e1f0d9c0902d447f8c810f5656c7271
OBN1 <nyangaresibrian@gmail.com>
2023-05-23 12:32:35 -0700

    Added the main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1
+ */
+int _putchar(char c);
+
+/**
+ * print_putchar - prints _putchar followed by a new line
+ */
+void print_putchar(void);
+
+#endif /* MAIN_H */
+
