1ea0644032af8b194f3a3e21be1397c6d1927e2b
OBN1 <nyangaresibrian@gmail.com>
2023-05-28 03:34:07 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
+
