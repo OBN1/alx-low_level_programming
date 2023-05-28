f35a1b82ae4f4e5ade1e90ca29b49a5009e54ba8
OBN1 <nyangaresibrian@gmail.com>
2023-05-28 04:38:20 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    printf("       *   \n");
+    printf("     *  **  \n");
+    printf("   *   *  *  \n");
+    printf(" *     *   *  \n");
+    printf("*** ** * *** \n");
+    printf("  * *   * *   \n");
+    printf("    *   *     \n");
+    printf("\n");
+
+    return (0);
+}
+
