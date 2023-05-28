b4c866a83e2af25a53ed9bfdb5df03f3b6ed22d0
OBN1 <nyangaresibrian@gmail.com>
2023-05-28 08:53:37 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,35 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, return the number of bytes written.
- * On error, return -1 and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - main
+* print putchar followed by newline
+* Return: 0
+*/
 int main(void)
 {
-	char *str = "_putchar";
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
-	return 0;
+	return (0);
 }
-
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* main - calls the rest of the functions
+* Return: 0
+*/
+int main(void)
+{
+
+	return (0);
+}
