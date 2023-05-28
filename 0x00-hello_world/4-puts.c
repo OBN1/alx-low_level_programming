2fafd7a396c047faa1a635d258bb0717b96fd718
OBN1 <nyangaresibrian@gmail.com>
2023-05-28 03:16:51 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-echo 'int main() {puts("Programming is like building a multilingual puzzle"); return 0;}' > program.c && gcc program.c -o program && ./program && echo 0
+#include <stdio.h>
 
+int main() {
+    puts("Programming is like building a multilingual puzzle.");
+    return 0;
+}
