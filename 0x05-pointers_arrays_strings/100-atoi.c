c14cc518b3c792befd2a78b04a23b1ccfd519fa8
OBN1 <nyangaresibrian@gmail.com>
2023-05-29 06:04:11 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.k"
+/**
+ * _atoi - Converting a string to integer
+ * @s: char array string
+ * Return: first integer
+ */
+int _atoi(char *s)
+{
+	int e;
+
+	int k, m;
+
+	k = 0;
+	m = -1;
+	for (e = 0; s[e] != '\0'; e++)
+	{
+		if (s[e] == '-')
+			m *= -1;
+
+		if (s[e] > 47 && s[e] < 58)
+		{
+			if (k < 0)
+				k = (k * 10) - (s[e] - '0');
+
+			else
+				k = (s[e] - '0') * -1;
+
+			if (s[e + 1] < 48 || s[e + 1] > 57)
+				break;
+		}
+	}
+	if (m < 0)
+		k *= -1;
+
+	return (k);
+}
+
