edf5bd60b099ec80ccae06eed06fa26589fea5b4
OBN1 <nyangaresibrian@gmail.com>
2023-05-29 05:01:36 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - printing string in reverse
+ * @s: char type string
+ */
+void print_rev(char *s)
+{
+	int e = 0;
+
+	while (s[e] != '\0')
+		e++;
+
+	while (e >= 0)
+	{
+		_putchar(s[e]);
+		e--;
+	}
+
+	_putchar('\n');
+}
