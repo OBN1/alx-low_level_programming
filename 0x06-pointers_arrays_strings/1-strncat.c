6ba84395b1b14e0531f06d6137fd3957b0fce9d9
OBN1 <nyangaresibrian@gmail.com>
2023-05-31 00:18:43 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strncat -  concatenates two strings
+ *@dest: Pointing to an char that will be updated
+ *@src: Pointer to an char that will be updated
+ *@n: value
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0') 
+	{
+		ptr++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+
+	*ptr = ('\0');
+
+    return (dest);
+}
