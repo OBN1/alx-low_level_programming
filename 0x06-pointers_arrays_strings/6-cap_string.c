bd4ba555c2555d7f3bd9ef0c83f429f16c0fa255
OBN1 <nyangaresibrian@gmail.com>
2023-05-31 10:15:41 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,61 @@
 #include "main.h"
-/**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
-char *cap_string(char *str)
-{
-	int i = 0;
-	int capitalize_next = 1;
 
-	while (str[i] != '\0')
+/**
+ * separator - checks if character is a seperator
+ * @c: character to be checked
+ * Return: if seperator return 1. Otherwise return 0;
+ */
+
+int separator(char c)
+{
+	/*Delcaring condition switch*/
+	switch (c)
 	{
-		if
-			(str[i] == ' ' ||
-			str[i] == '\t' ||
-			str[i] == '\n' ||
-			str[i] == ',' ||
-			str[i] == ';' ||
-			str[i] == '.' ||
-			str[i] == '!' ||
-			str[i] == '?' ||
-			str[i] == '"' ||
-			str[i] == '(' ||
-			str[i] == ')' ||
-			str[i] == '{' ||
-			str[i] == '}')
-			{
-			capitalize_next = 1;
-			}
-		else if (capitalize_next && islower(str[i]))
-		{
-			str[i] = toupper(str[i]);
-			capitalize_next = 0;
-		}
-		i++;
+	case ' ':
+	case '\t':
+	case '\n':
+	case ',':
+	case ';':
+	case '.':
+	case '!':
+	case '?':
+	case '"':
+	case '(':
+	case ')':
+	case '{':
+	case '}':
+		return (1);
+
+	default:
+		return (0);
 	}
 
-	return (str);
 }
+/**
+ * cap_string - capitalizes chars after given deliminators
+ * @s: string to uppercase
+ * Return: returns modified string
+ */
+char *cap_string(char *s)
+{
+	int count, upper;
+
+	upper = -32; /*value constant 32*/
+
+	count = 0;
+	/*Start WHILE*/
+	while (s[count] != '\0')
+	{
+		/*letters lowercase*/
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			/*Convert uppercase*/
+			if (s[count] == *s || separator(s[count - 1]))
+
+				s[count] += upper;
+		}
+		count++; /*Add count*/
+	}
+	return (s);
+}
+
