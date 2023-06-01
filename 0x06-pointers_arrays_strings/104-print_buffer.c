909f40232cf18233e87674ba9a3d35e2aca9ac94
pauditicha <pauditicha@gmail.com>
2023-06-01 10:06:10 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02hhx", b[i + j]);
+			else
+				printf("  ");
+
+			if (j % 2 == 1)
+				printf(" ");
+		}
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (b[i + j] >= 32 && b[i + j] <= 126)
+					printf("%c", b[i + j]);
+				else
+					printf(".");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+
+		printf("\n");
+	}
+}
