d3cb1d657e5ff2f9f8c44e4ba3be67cd23ec07f6
pauditicha <pauditicha@gmail.com>
2023-06-07 02:56:29 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _put_recursion - Prints a string followed by a new line
+ * @s: The string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	int k=o;
+
+	while (s[k] !='\0')
+	{
+		_putchar([k]);
+		k++;
+	}
+	_putchar('\n');
+}
