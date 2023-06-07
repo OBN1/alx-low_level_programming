10ad1496b3f0c5abf96c951bfa8474c1afb594f6
pauditicha <pauditicha@gmail.com>
2023-06-07 15:04:27 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * wildcmp - compare two strings for a wildcard char
+ * @s1: string being compared
+ * @s2: string to be compared against
+ *
+ * Return: 1 if considered indentical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 _ 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+
+	return (0);
+}
