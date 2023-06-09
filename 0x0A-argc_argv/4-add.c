c67a5c16022be8108beba28e4bc3bf37f9aae92d
pauditicha <pauditicha@gmail.com>
2023-06-09 07:54:22 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Program that adds positive numbers followed by a new line
+ * @argc: Argument count
+ * @argv: arguument vector
+ * return: Return 0 on succsess, otherwise return 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, y, r, s;
+
+	if (argc > 0)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			for (b = 0; argv[a][b] != 0; b++)
+			{
+				if (argv[a][b] < 48 || argv[a][b] > 57)
+				{
+					puts("Error");
+					return (1);
+				}
+			}
+		}
+	for (y = 1; y < argc; y++)
+	{
+		r = atoi(*(argv + y));
+		s += r;
+	}
+	printf("%d\n", s);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
