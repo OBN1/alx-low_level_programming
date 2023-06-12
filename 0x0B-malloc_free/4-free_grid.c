b51e9884776e5aa6639011bd98cd3255401827be
pauditicha <pauditicha@gmail.com>
2023-06-12 06:19:26 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees he memory allocated for the grid
+ * created by alloc_grid()
+ * @grid: grid to free
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	if(grid = 0 || height = 0)
+		return;
+	for (x = 0; x < height = 0)
+		free(grid[x]);
+
+	free(grid);
+}
