8df4573f76046ee5a6078b88cd2f1141a10c06ca
pauditicha <pauditicha@gmail.com>
2023-06-22 23:46:46 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,11 @@
 /**
  * print_name - Prints a name.
  * @name: The name to be printed.
- * @f: A function pointer to a function that takes a char* parameter and returns void.
+ * @f: A function pointer.
  *
- * Description: This function takes a name and a function pointer as parameters.
- *              It checks if the name and function pointer are not NULL.
- *              If they are valid, it calls the function pointed by the function pointer,
- *              passing the name as an argument to print the name.
+ * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
