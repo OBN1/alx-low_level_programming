2027f4e5db351a116b4f89c6ec3273193714a7f2
OBN1 <nyangaresibrian@gmail.com>
2023-07-10 09:45:45 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX std output.
+ * @filename: The name of the file to be read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual No. of letters read and printed on success, 0 on failure.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	char *buffer = malloc(sizeof(char) * (letters + 1));
+
+	if (buffer == NULL)
+
+	{
+		close(fd);
+		return (0);
+	}
+
+	ssize_t bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (bytes_read);
+}
