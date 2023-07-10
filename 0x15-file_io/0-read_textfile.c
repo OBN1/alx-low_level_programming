5719869b171f99009e51a574ac7e09765d543e22
OBN1 <nyangaresibrian@gmail.com>
2023-07-10 09:51:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,45 +8,30 @@
  * Return: The actual No. of letters read and printed on success, 0 on failure.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-
-	if (buffer == NULL)
-
-	{
-		close(fd);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	ssize_t bytes_read = read(fd, buffer, letters);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	if (bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	free(buffer);
 	close(fd);
 
-	return (bytes_read);
+	free(buf);
+
+	return (nwr);
 }
